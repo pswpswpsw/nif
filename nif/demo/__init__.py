b9b09a7866303b1e823d59aa6b8e65b91186b5ca
shaowu <shawnpan@umich.edu>
2022-04-20 17:10:25 -0400

    add

--- nif/demo/__init__.py
+++ nif/demo/__init__.py
@@ -1,9 +1,11 @@
 from .traveling_wave import TravelingWave
 from .traveling_wave_high_freq import TravelingWaveHighFreq
 from .cylinderflow import CylinderFlow
+from .point_wise_data import PointWiseData
 
 __all__ = [
     "TravelingWave",
     "TravelingWaveHighFreq",
     "CylinderFlow",
+    "PointWiseData"
 ]

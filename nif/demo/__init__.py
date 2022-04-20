ce74904e32e52fc649f10ca7d3c47436732e55ec
shaowu <shawnpan@umich.edu>
2022-04-20 17:01:28 -0400

    add

--- nif/demo/__init__.py
+++ nif/demo/__init__.py
@@ -1,8 +1,9 @@
-# from . import TravelingWaveHighFreq
-# from cylinderflow import CylinderFlow
+from traveling_wave import TravelingWave
+from traveling_wave_high_freq import TravelingWaveHighFreq
+from cylinderflow import CylinderFlow
 
-# __all__ = [
-#     "CylinderFlow",
-#     "TravelingWave",
-#     "TravelingWaveHighFreq"
-# ]
+__all__ = [
+    "TravelingWave",
+    "TravelingWaveHighFreq",
+    "CylinderFlow",
+]

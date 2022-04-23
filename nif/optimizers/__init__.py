5b8d43134bfc5d6d3782877a483987de1c508eb8
shaowu <shawnpan@umich.edu>
2022-04-23 16:52:34 -0400

    add

--- nif/optimizers/__init__.py
+++ nif/optimizers/__init__.py
@@ -1,12 +1,12 @@
 from tensorflow_probability.python.optimizer import lbfgs_minimize
 from .lbfgs import function_factory, TFPLBFGS
 from .lbfgs_V2 import LBFGSOptimizer
-from .L4 import L4Adam
+from .L4 import SGOptimizer
 
 __all__ = [
     "function_factory",
     "lbfgs_minimize",
     "LBFGSOptimizer",
     "TFPLBFGS",
-    "L4Adam"
+    "SGOptimizer"
 ]

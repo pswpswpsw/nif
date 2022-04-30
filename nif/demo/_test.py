f771d1c4e8ff2df25375dcb96f4e9209f549f1fe
shaowu <shawnpan@umich.edu>
2022-04-30 14:32:27 -0700

    add

---
+++ nif/demo/_test.py
@@ -0,0 +1,8 @@
+from point_wise_data import PointWiseData
+import numpy as np
+
+data = np.random.rand(100,3)
+normalized_data, mean, std = PointWiseData.minmax_normalize(raw_data=data, n_para=1, n_x=1, n_target=1)
+
+print(normalized_data.min(axis=0))
+print(normalized_data.max(axis=0))

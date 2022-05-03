fd334c1319927041f469899a029ee86a5111ad73
shaowu <shawnpan@umich.edu>
2022-05-03 11:25:38 -0700

    add a few things

--- nif/__init__.py
+++ nif/__init__.py
@@ -3,9 +3,9 @@ from .__about__ import __version__
 import tensorflow as tf
 from tensorflow.keras import mixed_precision
 
-from .model import *
-from . import demo
-from . import data
+from nif.model import *
+from nif import demo
+from nif import data
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if len(gpus) > 0:
@@ -15,6 +15,7 @@ if len(gpus) > 0:
     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
 
 __all__ = [
+    "data",
     "tf",
     "NIFMultiScale",
     "NIFMultiScaleLastLayerParameterized",
@@ -22,5 +23,4 @@ __all__ = [
     "mixed_precision",
     "optimizers",
     "demo",
-    "data"
 ]
